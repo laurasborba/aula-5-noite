925eae58d2d20a76e8b24bf75cb4a68b3ae82fdb
Laura Santos <142456003+laurasborba@users.noreply.github.com>
2023-08-21 21:43:45 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+/*
+* Laura Santos
+* Primeiro comentario
+*/
+console.log("Olá mundo!");
+//prompt("Digite seu nome");
+
+//Variaveis
+const variavelA = 100;
+const variavelB = variavelA + 50;
+console.log(variavelB);
+
+let numero = 50;
+console.log(numero);
+numero = 80;
+console.log(numero);
+
+
+const nome = "Laura";
+const idade = 25;
+
+console.log("Olá!", "Meu nome é", nome, "e eu tenho", idade, "anos.");
+
+
+const primeiro = "Laura";
+const sobrenome = "Santos";
+const idades = 25;
+const estuda = "estudante";
+
+
+console.log("Ola! meu nome é", primeiro, sobrenome, "eu tenho", idades, "anos", "eu sou", estuda);   
